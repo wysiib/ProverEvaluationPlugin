43e3218feb2f47d72a0f116fad2b31e1f53fb07c
Sebastian Krings <sebastian@krin.gs>
2014-02-14 10:36:09 +0100

    working on csv export: dialogs

--- de.provereval/src/de/provereval/output/ResultDialog.java
+++ de.provereval/src/de/provereval/output/ResultDialog.java
@@ -1,9 +1,12 @@
 package de.provereval.output;
 
+import java.io.*;
 import java.util.*;
 import java.util.List;
 
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.*;
 import org.eclipse.swt.widgets.*;
 
 import de.provereval.ProverEvaluationTask;
@@ -25,6 +28,41 @@ public class ResultDialog extends Dialog {
 
 		final ResultTableViewer viewer = new ResultTableViewer(body, grouped);
 
+		final Button exportButton = new Button(body, SWT.PUSH);
+		exportButton.setText("Export to CSV");
+		exportButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				export(parent);
+			}
+		});
+
 		return body;
+	}
+
+	private void export(Composite parent) {
+		FileDialog fd = new FileDialog(parentShell, SWT.SAVE);
+		fd.setText("Export to CSV");
+		String[] filterExt = { "*.csv" };
+		fd.setFilterExtensions(filterExt);
+		String path = fd.open();
+		exportToCSVFile(path);
+	}
+
+	private void exportToCSVFile(String path) {
+		Writer writer = null;
+		try {
+			writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(path)));
+			writer.write("Something");
+		} catch (IOException ex) {
+			// report
+		} finally {
+			try {
+				writer.close();
+			} catch (Exception ex) {
+			}
+		}
+
 	}
 }
