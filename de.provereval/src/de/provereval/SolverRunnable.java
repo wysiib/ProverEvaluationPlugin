bbd7081d4e95833a328880df48f1a03499bc2634
Sebastian Krings <sebastian@krin.gs>
2015-02-12 11:57:13 +0100

    use execution pool in a non-blocking way, wait for futures to manifest while updating progress bar

--- de.provereval/src/de/provereval/SolverRunnable.java
+++ de.provereval/src/de/provereval/SolverRunnable.java
@@ -28,16 +28,22 @@ public class SolverRunnable implements IRunnableWithProgress {
 	@Override
 	public void run(IProgressMonitor monitor) throws InvocationTargetException,
 			InterruptedException {
+		List<Future<ProverEvaluationResult>> futures = new ArrayList<Future<ProverEvaluationResult>>();
 		monitor.beginTask("Running Provers", tasks.size());
 
 		int cores = Runtime.getRuntime().availableProcessors();
+		if (cores != 1) {
+			cores--; // do not use all cores if possible to avoid blocking
+		}
 		ExecutorService pool = Executors.newFixedThreadPool(cores);
 
-		List<Future<ProverEvaluationResult>> invokeAll = pool.invokeAll(tasks);
+		for (ProverEvaluationTask task : tasks) {
+			futures.add(pool.submit(task));
+		}
 
 		for (int i = 0; i < tasks.size(); i++) {
 			try {
-				results.add(invokeAll.get(i).get());
+				results.add(futures.get(i).get());
 			} catch (ExecutionException e) {
 				canceled = true;
 				monitor.done();
@@ -45,7 +51,8 @@ public class SolverRunnable implements IRunnableWithProgress {
 			}
 
 			monitor.worked(1);
-			monitor.setTaskName("Proof Obligation " + i + " of " + tasks.size());
+			monitor.setTaskName("Proof Obligation " + i + 1 + " of "
+					+ tasks.size());
 
 			if (monitor.isCanceled()) {
 				canceled = true;
